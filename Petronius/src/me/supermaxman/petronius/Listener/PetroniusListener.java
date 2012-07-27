055fe4077cd621b43df1bd567dae4733854ffeb6
Max <supermaxman1@gmail.com>
2012-07-27 15:19:12 -0500

    Added item pickup fix

--- Petronius/src/me/supermaxman/petronius/Listener/PetroniusListener.java
+++ Petronius/src/me/supermaxman/petronius/Listener/PetroniusListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.util.Vector;
 
@@ -32,21 +33,16 @@ public class PetroniusListener implements Listener{
 	
 	@EventHandler
 	public void onPlayerDisconnect(PlayerQuitEvent event){
-		final Player p = event.getPlayer();
-		
-		for(Pet pet : Petronius.pets){	
-			if(pet.getOwner().equalsIgnoreCase(p.getName())){
-			
-           	pet.getItem().setVelocity(new Vector(
-         	(p.getLocation().add(0, height, 0).toVector().subtract(pet.getItem().getLocation().toVector())).getX()*3
-        	,0.01
-          	,(p.getLocation().add(0, height, 0).toVector().subtract(pet.getItem().getLocation().toVector())).getZ()*3));
-          	break;
-			}
-		}
 		
 	}
 	
 	
-	
+	@EventHandler
+	public void onPlayerPickupItem(PlayerPickupItemEvent event){
+		for(Pet pet : Petronius.pets){	
+			if(pet.getItem().getUniqueId().equals(event.getItem().getUniqueId())){
+				event.setCancelled(true);
+			}
+		}
+	}
 }
